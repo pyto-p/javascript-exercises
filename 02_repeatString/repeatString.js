0d0f0dd225cda5f7190c11cbbb88e4c30f906c68
pyto-p <sbstntmprl@gmail.com>
2023-05-30 19:04:01 +0800

    finished repeatString exercise.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    ogWord = word;
+    if (num >= 1) {
+        for (i = 1; i < num; i++) {
+            word = word + ogWord;
+        }
+        return word;
+    } else if (num == 0 && ogWord.trim != '') {
+        return "";
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
